840f8d37a0489d8fa0ff83f2638b791ebe638897
Wellington Silva de Araújo <wsa.jpb@gmail.com>
2024-07-13 11:10:15 -0300

    Add log ao publicar no kafka

--- acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/kafka/producer/MessageProducer.java
+++ acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/kafka/producer/MessageProducer.java
@@ -2,16 +2,22 @@ package br.com.acmepay.adapters.output.kafka.producer;
 
 import br.com.acmepay.application.requests.TransactionRequest;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import static br.com.acmepay.config.RabbitConfig.QUEUE_CHECK_DOCUMENT;
+
 @Component
 @AllArgsConstructor
+@Slf4j
 public class MessageProducer {
     private final KafkaTemplate<String, TransactionRequest> kafkaTemplate;
 
     public String send(String topic, TransactionRequest transactionRequest) {
+        log.info("Publishing Kafka: Topic {} / TransactionRequest {}", topic, transactionRequest);
         kafkaTemplate.send(topic, transactionRequest);
+        log.info("Published Kafka: Topic {} / TransactionRequest {}", topic, transactionRequest);
         return "send ok";
     }
 }
