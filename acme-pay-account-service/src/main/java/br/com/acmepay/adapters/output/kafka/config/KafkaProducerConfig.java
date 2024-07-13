d5a0c24b89142008986228616c8a7dc2dc941798
Wellington Silva de Araújo <wsa.jpb@gmail.com>
2024-07-13 13:38:29 -0300

    Add propertie bootstrap-servers

--- acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/kafka/config/KafkaProducerConfig.java
+++ acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/kafka/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package br.com.acmepay.adapters.output.kafka.config;
 import br.com.acmepay.application.requests.TransactionRequest;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,10 +16,14 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
     @Bean
     public ProducerFactory<String, TransactionRequest> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
